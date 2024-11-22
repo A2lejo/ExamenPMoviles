fe716b940880497b0f25a0c492125569dceced6c
Alejandro <alejandro27campoverde@gmail.com>
2024-11-22 16:50:09 -0500

    App

--- Examen/src/app/app.module.ts
+++ Examen/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment'; // Asegúrate de que la ruta sea correcta
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
--- Examen/src/environments/environment.prod.ts
+++ Examen/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCIUFnhYToVPmsFQ7HEmycxWNH9FFZQf1Y",
+    authDomain: "aplicaciones-moviles-clases.firebaseapp.com",
+    projectId: "aplicaciones-moviles-clases",
+    storageBucket: "aplicaciones-moviles-clases.firebasestorage.app",
+    messagingSenderId: "80636065798",
+    appId: "1:80636065798:web:4b5e48ce0b1943b682ab87",
+    measurementId: "G-2XCZ9D19M2"
+  }
 };
